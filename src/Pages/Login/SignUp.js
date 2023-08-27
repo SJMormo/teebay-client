3b71620794eb2575b1c0f7f7cb156f8e6ffa2a91
SJMormo <sjm91097@gmail.com>
2023-08-28 01:24:24 +0600

    user mutation

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -3,11 +3,24 @@ import { Button, FloatingLabel, Form } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
+import { useMutation, gql } from "@apollo/client";
+
+const CREATE_USER = gql`
+  mutation CreateUser($name: String!) {
+    createUser(name: $name) {
+      id
+      name
+    }
+  }
+`;
 
 const SignUp = () => {
   const navigate = useNavigate();
+  const nameRef = useRef("");
   const emailRef = useRef("");
   const passwordRef = useRef("");
+
+  const [createUser] = useMutation(CREATE_USER);
 
   const [createUserWithEmailAndPassword, user, loading, error] =
     useCreateUserWithEmailAndPassword(auth);
@@ -16,17 +29,42 @@ const SignUp = () => {
     navigate("/signin");
   }
 
-  const handleRegister = (event) => {
+  const handleRegister = async (event) => {
     event.preventDefault();
+    const name = nameRef.current.value;
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
-    createUserWithEmailAndPassword(email, password);
-    console.log(email, password);
+    // createUserWithEmailAndPassword(email, password);
+    console.log(name, email, password);
+
+    try {
+      const { data } = await createUser({
+        variables: { name },
+      });
+  
+      console.log("User created:", data.createUser);
+      navigate("/signin");
+    } catch (error) {
+      console.error("Error creating user:", error);
+    }
   };
 
   return (
     <div>
       <Form onSubmit={handleRegister} className="w-50 mx-auto mt-3">
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Name"
+          className="mb-3"
+        >
+          <Form.Control
+            ref={nameRef}
+            type="text"
+            placeholder="Name"
+            required
+          />
+        </FloatingLabel>
+
         <FloatingLabel
           controlId="floatingInput"
           label="Email address"
@@ -66,3 +104,4 @@ const SignUp = () => {
 };
 
 export default SignUp;
+
