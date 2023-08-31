8b626f0ddb3e741776e12615eb08fd2d6c6c3e57
SJMormo <sjm91097@gmail.com>
2023-09-01 03:30:06 +0600

    user id retrieved

--- src/Pages/AddProduct/MainForm.js
+++ src/Pages/AddProduct/MainForm.js
@@ -1,6 +1,10 @@
 import FormInputs from "./FormInputs";
 import useFormContext from "../../Hooks/useFormContext";
 import { Button, Col, Form, Row } from "react-bootstrap";
+import { useQuery } from "@apollo/client";
+import { GET_USERS } from "../../graphql/queries";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MainForm = () => {
   const {
@@ -17,8 +21,27 @@ const MainForm = () => {
   } = useFormContext();
 
   const handlePrev = () => setPage((prev) => prev - 1);
-
   const handleNext = () => setPage((prev) => prev + 1);
+
+  // Getting current user id
+  const {
+    loading: loadingUsers,
+    error: usersError,
+    data: usersData,
+  } = useQuery(GET_USERS);
+  const [user, loading] = useAuthState(auth);
+
+  if (loadingUsers || loading) return <p>Loading...</p>;
+  if (usersError) return <p>Error: {usersError.message}</p>;
+
+  const targetEmail = user?.email;
+
+  const userWithTargetEmail = usersData.users.find(
+    (product) => product.email === targetEmail
+  );
+
+  const userId = userWithTargetEmail.id
+  console.log(userId);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -31,40 +54,40 @@ const MainForm = () => {
       {/* <h2>{page}</h2> */}
 
       <FormInputs />
-      
-        <Row>
-          <Col className="d-flex justify-content-evenly">
-            <Button
-              type="button"
-              className={`button ${prevHide}`}
-              onClick={handlePrev}
-              disabled={disablePrev}
-            >
-              Prev
-            </Button>
-          </Col>
-          <Col className="d-flex justify-content-evenly">
-            {nextHide ? (
-              <></>
-            ) : (
-              <Button type="button" onClick={handleNext} disabled={disableNext}>
-                Next
-              </Button>
-            )}
 
-            {submitHide ? (
-              <></>
-            ) : (
-              <Button
-                type="submit"
-                className={`button ${submitHide}`}
-                disabled={!canSubmit}
-              >
-                Submit
-              </Button>
-            )}
-          </Col>
-        </Row>
+      <Row>
+        <Col className="d-flex justify-content-evenly">
+          <Button
+            type="button"
+            className={`button ${prevHide}`}
+            onClick={handlePrev}
+            disabled={disablePrev}
+          >
+            Prev
+          </Button>
+        </Col>
+        <Col className="d-flex justify-content-evenly">
+          {nextHide ? (
+            <></>
+          ) : (
+            <Button type="button" onClick={handleNext} disabled={disableNext}>
+              Next
+            </Button>
+          )}
+
+          {submitHide ? (
+            <></>
+          ) : (
+            <Button
+              type="submit"
+              className={`button ${submitHide}`}
+              disabled={!canSubmit}
+            >
+              Submit
+            </Button>
+          )}
+        </Col>
+      </Row>
     </Form>
   );
 
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,7 +1,17 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const GET_USER_ID_BY_EMAIL = gql`
   query GetUserIdByEmail($email: String!) {
     getUserIdByEmail(email: $email)
+  }
+`;
+
+export const GET_USERS = gql`
+  query {
+    users {
+      id
+      name
+      email
+    }
   }
 `;
